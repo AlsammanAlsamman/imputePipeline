996d3d7dc5854f4942c1bcfafaf256d9105f3db3
AlsammanAlsamman <smahmoud@ageri.sci.eg>
2025-11-24 14:49:24 -0600

    new

--- steps.sh
+++ steps.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Input QC
-./submit.sh --snakefile rules/input_qc.smk results/01_pre_qc/MEX123_input_qc.done
+# Dataset name from config
+dataset="MEX"
+./submit.sh --snakefile rules/input_qc.smk results/01_pre_qc/${dataset}_input_qc.done
 
 # Remove Ambiguous SNPs
-./submit.sh --snakefile rules/remove_ambiguous.smk results/02_remove_ambiguous/MEX123_remove_ambiguous.done
+./submit.sh --snakefile rules/remove_ambiguous.smk results/02_remove_ambiguous/${dataset}_remove_ambiguous.done
 
 # VCF Convert (all chromosomes 1-22)
-./submit.sh --snakefile rules/vcf_convert.smk results/03_vcf/MEX123_convertvcf.done
+./submit.sh --snakefile rules/vcf_convert.smk results/03_vcf/${dataset}_convertvcf.done
 
 # Harmonize VCF against reference (parallel by chromosome)
-./submit.sh --snakefile rules/harmonize_vcf.smk --jobs 22 --cores 44 results/04_harmonize/MEX123_harmonize.done
+./submit.sh --snakefile rules/harmonize_vcf.smk --jobs 22 --cores 44 results/04_harmonize/${dataset}_harmonize.done
 
 # Download imputed results from server
-./submit.sh --snakefile rules/download_imputed.smk results/05_imputed_data/MEX123_download_imputed.done
+./submit.sh --snakefile rules/download_imputed.smk results/05_imputed_data/${dataset}_download_imputed.done
 
 # Extract password-protected imputed files (parallel by chromosome)
-./submit.sh --snakefile rules/extract_imputed.smk --jobs 22 --cores 22 results/06_extracted_data/MEX123_extract_imputed.done
+./submit.sh --snakefile rules/extract_imputed.smk --jobs 22 --cores 22 results/06_extracted_data/${dataset}_extract_imputed.done
 
 # Filter imputed data from Michigan server (parallel by chromosome)
 ./submit.sh --snakefile rules/filter_imputed_michigan.smk --jobs 22 results/07_filtered/michigan_filtering_complete.done
@@ -28,7 +29,7 @@
 ./submit.sh --snakefile rules/convert_to_plink_all.smk --jobs 22 results/09_plink_format/plink_conversion_complete.done
 
 # Merge chromosome PLINK binaries into a single dataset
-./submit.sh --snakefile rules/merge_plink2.smk results/10_plink_merged/MEX123_merged.done
+./submit.sh --snakefile rules/merge_plink2.smk results/10_plink_merged/${dataset}_merged.done
 
 # Convert merged PLINK2 dataset into PLINK1 binary format
-./submit.sh --snakefile rules/convert_plink2_to_bed.smk results/10_plink_merged/MEX123_bed.done
+./submit.sh --snakefile rules/convert_plink2_to_bed.smk results/10_plink_merged/${dataset}_bed.done
